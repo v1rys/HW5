7497e22577217b0f7da910c92b0f55c1042cbea7
v1rys <alaverdi@gmail.com>
2022-08-29 00:04:27 +0700

    Finish Task35

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,34 +4,34 @@
 
 // [345, 897, 568, 234] -> 2
 
-int [] MassivRandom (int size)
+int[] MassivRandom(int size)
 {
-   int [] array = new int [size];
-   Random rand = new Random();
-   
-   for(int i =0; i < size; i++)
-   {
-    array[i] = rand.Next(100,1000); 
-   }
-   return array;
-} 
+    int[] array = new int[size];
+    Random rand = new Random();
 
-int ShowEven (int [] array)
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(100, 1000);
+    }
+    return array;
+}
+
+int ShowEven(int[] array)
 {
-    int count =0;
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] %2 == 0)
+        if (array[i] % 2 == 0)
         {
             count++;
         }
     }
-    return count ;
-    
+    return count;
+
 }
 
 
-int [] arr = MassivRandom(10);
+int[] arr = MassivRandom(10);
 Console.WriteLine(string.Join(",", arr));
 int count = ShowEven(arr);
 Console.WriteLine(count);
---
+++ Task35/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+// Найдите количество элементов массива, значения которых лежат в 
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении
+//  сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] MassivRandom(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(1, 1000);
+    }
+    return array;
+}
+
+int FindTheNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 | array[i] <= 99)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] arr = MassivRandom(123);
+Console.Write(string.Join(",", arr));
+Console.WriteLine();
+int count = FindTheNumber(arr);
+Console.Write(count);
