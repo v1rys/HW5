731ca85ea924a6f1fccbfe87d30100dd34176062
v1rys <alaverdi@gmail.com>
2022-08-29 01:02:01 +0700

    Finised Task36

--- Task35/Program.cs
+++ Task35/Program.cs
@@ -24,7 +24,7 @@ int FindTheNumber(int[] array)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] >= 10 | array[i] <= 99)
+        if (array[i] >= 10 && array[i] <= 99)
         {
             count++;
         }
---
+++ Task36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными 
+// числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] MassivRandom(int size)
+{
+    int[] array = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rand.Next(1, 1000);
+    }
+    return array;
+}
+
+
+int SumOfElements(int[] array)
+{
+    int sum = 0;
+    for (int i = 2; i < array.Length; i += 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] arr = MassivRandom(10);
+Console.Write(string.Join(",", arr));
+Console.WriteLine();
+int sum = SumOfElements(arr);
+Console.WriteLine($"{sum}");
+
